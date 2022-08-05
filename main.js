2ba527fa094ff54af22df1878ed4d3002b40aca6
cristian_moscardini <cristian_moscardini@hotmail.com>
2022-08-04 22:38:49 -0300

    arrays, metodos y mapeo

--- main.js
+++ main.js
@@ -1,51 +1,118 @@
-let cocktail = prompt("Ingresa un cocktail y te mostraré su composición y costo\nCarta:\n- Gin Tonic\n- Caipirinha\n- Mojito \n(Escribe 'Exit' para salir)");
-precio=0;
-const IVA=1.21;
 
-while(cocktail!='Exit'){
-    switch(cocktail){
-        case "Gin Tonic":
-            precioConIva=parseInt((precio+300)*IVA);
-            alert("Composición: Gin, Agua tónica, hielo, piel de limón\nCosto: $"+precioConIva);
-            verificarMetodoPago();
-            break;
-            
-        case "Caipirinha":
-            precioConIva=parseInt((precio+500)*IVA);
-            alert("Composición: CachaÇa, exprimido de lima, syrup de lima, hielo\nCosto: $"+precioConIva);
-            verificarMetodoPago();
-            break;
-        
-        case "Mojito":
-            precioConIva=parseInt((precio+600)*IVA);
-            alert("Composición: Ron, exprimido de lima, azucar, hojas de menta, hielo molido, gotas de Angostura\nCosto: $"+precioConIva);
-            verificarMetodoPago();
-            break;
-        
-        default:
-            alert("Ese cocktail no se encuentra en la carta");
-            break;
-        
-        function verificarMetodoPago(){
-            let metodoDePago=prompt("Ingresa un método de pago");
-            if(metodoDePago=="Visa"){
-                let precioConDesc=calcularDescuento(precioConIva,20);
-                alert("Precio con descuento: $"+precioConDesc);
-            
-            }else{
-                alert("No posee descuento.\nEl costo final es: $"+precioConIva);
-            }
-        }
 
-        function calcularDescuento(precioConIva,descuento){
-            let desc=(precioConIva*descuento)/100;
-            let totConDesc = precioConIva - desc;
-            return totConDesc;
-        }
-    }
-    cocktail = prompt("Ingresa un cocktail y te mostraré su composición y costo \n(Escribe 'Exit' para salir)");
-    
+class Cocktail {
+  constructor(nombre, composición, precio) {
+    this.nombre = nombre;
+    this.composición = composición;
+    this.precio = precio;       
+  }
+
 }
-        
-        
+let listaCocktails = [];
 
+let cocktail1 = new Cocktail(
+  "Gin Tonic",
+  "Gin, Agua Tónica, Hielo, piel de Limón",
+  300
+);
+let cocktail2 = new Cocktail(
+  "Caipirinha",
+  "CachaÇa, Exprimido de Lima, Syrup de lima, Hielo",
+  500
+);
+let cocktail3 = new Cocktail(
+  "Mojito",
+  "Ron, Exprimido de Lima, Azucar, Hojas de menta, Hielo molido, Gotas de Angostura",
+  600
+);
+
+listaCocktails.push(cocktail1);
+listaCocktails.push(cocktail2);
+listaCocktails.push(cocktail3);
+
+let mensaje =
+  "Ingresa un cocktail y te mostraré su composición y costo\n(Escribe 'Exit' para salir)\nCarta:";
+
+for (const elemento of listaCocktails) {
+  mensaje += "\n- " + elemento.nombre;
+  
+}
+
+
+let cocktail = prompt(mensaje);
+
+
+const IVA = 1.21;
+let precioConIva=0;
+
+while (cocktail != "Exit") {
+  switch (cocktail) {
+    case "Gin Tonic":
+      precioConIva = parseInt(listaCocktails[0].precio * IVA);
+      alert(
+        "Composición: "+ listaCocktails[0].composición +"\nCosto: $" +
+          precioConIva
+      );
+      verificarMetodoPago();
+      break;
+
+    case "Caipirinha":
+      precioConIva = parseInt(listaCocktails[1].precio * IVA);
+      alert(
+        "Composición: "+ listaCocktails[1].composición +"\nCosto: $" +
+          precioConIva
+      );
+      verificarMetodoPago();
+      break;
+
+    case "Mojito":
+      precioConIva = parseInt(listaCocktails[2].precio * IVA);
+      alert(
+        "Composición: "+ listaCocktails[2].composición +"\nCosto: $" +
+          precioConIva
+      );
+      verificarMetodoPago();
+      break;
+    
+      //La "clave" ADMINISTRADOR la utiliza solo el dueño para incrementar los precios de la carta, por eso no aparece en el mensaje
+    case "Administrador":
+        let aumento=parseInt(prompt("Indique el porcentaje de aumento: "));
+        porc=aumento/100;
+      listaCocktails=listaCocktails.map(
+          (el)=>{
+              let cocktailAumentado=new Cocktail(el.nombre,el.composición,parseInt(el.precio*(1+porc)));
+              return cocktailAumentado;
+            }
+            
+        )
+        let preciosAumentados="Los nuevos precios para los cocktails son: \n";
+        for(const objeto of listaCocktails){
+            preciosAumentados+=objeto.nombre+" - $"+objeto.precio+"\n";
+
+        }
+        alert (preciosAumentados);
+        break;
+
+
+    default:
+      alert("Ese cocktail no se encuentra en la carta");
+      break;
+
+    }
+    cocktail = prompt(mensaje);
+}
+
+function verificarMetodoPago() {
+  let metodoDePago = prompt("Ingresa un método de pago");
+  if (metodoDePago == "Visa") {
+    let precioConDesc = calcularDescuento(precioConIva, 20);
+    alert("Precio con descuento: $" + precioConDesc);
+  } else {
+    alert("No posee descuento.\nEl costo final es: $" + precioConIva);
+  }
+}
+function calcularDescuento(precioConIva, descuento) {
+  let desc = (precioConIva * descuento) / 100;
+  let totConDesc = precioConIva - desc;
+  return totConDesc;
+}
